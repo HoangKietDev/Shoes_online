7b0bf1a476cc96e4f235164d1e88dcf168625d6d
fhnib <fhnibkma@gamil.com>
2025-05-17 23:04:50 +0700

    config tailwind, start create UI

---
+++ src/components/NavComponent/NewAndFeatured.vue
@@ -0,0 +1,65 @@
+<script setup></script>
+
+<template>
+  <div class="bg-white w-full h-[380px]">
+    <div class="flex justify-center items-center h-full">
+      <ul class="flex space-x-20">
+        <li>
+          <span class="font-semibold mb-2 inline-block">Featuredddd</span>
+          <p class="font-semibold"><a href="">Shop All New Arrivals</a></p>
+          <p><a href="">Best Sellers</a></p>
+          <p><a href="">SNKRS Launch Calendar</a></p>
+          <p><a href="">Air Max Home</a></p>
+          <p><a href="">Nike Gift Guide</a></p>
+        </li>
+        <li>
+          <span class="font-semibold mb-2 inline-block">Shop Icons</span>
+          <p><a href="">Air Force 1</a></p>
+          <p><a href="">Air Jordan 1</a></p>
+          <p><a href="">Air Max</a></p>
+          <p><a href="">Dunk</a></p>
+          <p><a href="">Blazer</a></p>
+          <p><a href="">Pegasus</a></p>
+          <p><a href="">Mercurial</a></p>
+        </li>
+        <li>
+          <span class="font-semibold mb-2 inline-block">Jordan</span>
+          <p><a href="">Shop All Jordan</a></p>
+          <p><a href="">New this Month</a></p>
+          <p><a href="">Jordan Streetwear</a></p>
+          <p><a href="">Jordan Basketball</a></p>
+          <p><a href="">Jordan Golf</a></p>
+          <p><a href="">Jordan x PSG</a></p>
+        </li>
+        <li>
+          <span class="font-semibold mb-2 inline-block">Discover Sport</span>
+          <p><a href="">Football</a></p>
+          <p><a href="">Running</a></p>
+          <p><a href="">Basketball</a></p>
+          <p><a href="">Training and Gym</a></p>
+          <p><a href="">Golf</a></p>
+          <p><a href="">Tennis</a></p>
+          <p><a href="">Yoga</a></p>
+          <p><a href="">Dance</a></p>
+          <p><a href="">Skateboarding</a></p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+span {
+  font-size: 14px;
+}
+p {
+  font-size: 14px;
+  color: #000;
+  opacity: 60%;
+  margin-bottom: 5px;
+}
+p:hover {
+  color: #000;
+  opacity: 100%;
+}
+</style>
---
+++ src/page/Homepage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+// TypeScript enabled
+import Navbar from '@/components/Navbar.vue'
+import { ref } from 'vue'
+Navbar
+</script>
+
+<template>
+  <!-- type checking and auto-completion enabled -->
+  <header>
+    <!-- <Navbar /> -->
+    <div class="block text-red-50 bg-red-500">This should show at ≥640px</div>
+  </header>
+  <main></main>
+</template>
